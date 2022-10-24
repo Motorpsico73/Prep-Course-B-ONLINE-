44488c1c334d04e58c847a71781b56232b630995
Motorpsico73 <nico_12_06_@hotmail.com>
2022-10-24 16:34:15 -0300

    Test pasados Ok!

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,48 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acu = 0;
+    for ( var i  = 1; i < 11; i++){
+      acu = acu+i;
+    } return acu;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var par= [];
+  var cero = 0;
+  while (cero < array.length){
+    if (array[cero] % 2 === 0){
+      par.push(array[cero])
+    } cero ++;
+  } return par
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+ var cuadrado = array.map(function(ele){
+  return Math.pow(ele,2);
+ }); return cuadrado
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var sumar = array.reduce(function (ele, ind){
+    return ele + ind
+  }); return sumar
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var digi = num +''
+  return digi.length
 }
   
   
